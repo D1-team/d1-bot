80cbbdcb3481fd161689c180e6aa513c31bb0969
ivandiaznoriega <angel.diaz@live.com.mx>
2021-09-19 15:29:28 -0500

    formats code

--- cogs/fun.py
+++ cogs/fun.py
@@ -33,7 +33,7 @@ class Fun(commands.Cog, name="fun"):
         # This will prevent your bot from stopping everything when doing a web request - see: https://discordpy.readthedocs.io/en/stable/faq.html#how-do-i-make-a-web-request
         async with aiohttp.ClientSession() as session:
             async with session.get(
-                    "https://uselessfacts.jsph.pl/random.json?language=en"
+                "https://uselessfacts.jsph.pl/random.json?language=en"
             ) as request:
                 if request.status == 200:
                     data = await request.json()
